c230d53713d0540e3cb5015cf130f86281dc6e62
Gemma Batlle <gembb15@gmail.com>
2022-09-30 13:13:59 +0200

    sesion-14

---
+++ index.js
@@ -0,0 +1,10 @@
+const boton = document.getElementById("btn")
+boton.addEventListener("click", () => {
+    alert("click en el botón")
+})
+
+$(() => {
+    $("#btn").click(() => {
+        console.log("Hola, estoy utilizando JQuery")
+    })
+})
